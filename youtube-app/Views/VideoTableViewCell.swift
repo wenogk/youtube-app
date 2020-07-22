d042547b1b6391c21f6fbbe0e39d9aad438cd22f
wenogk <drf325@nyu.edu>
2020-07-22 17:43:06 +0400

    organized files + new custom table view cell class for video

---
+++ youtube-app/Views/VideoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoTableViewCell.swift
+//  youtube-app
+//
+//  Created by Romeno Wenogk Fernando on 22/07/2020.
+//  Copyright © 2020 Romeno Wenogk Fernando. All rights reserved.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
